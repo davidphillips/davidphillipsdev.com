a732b9dc08dc33529312585598f1975e5f626cc9
David <davidphillips92@gmail.com>
2018-09-08 14:42:47 +1000

    added container

---
+++ src/components/container.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default ({ children }) => (
+  <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}>{children}</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
 
-export default () =>
-<div style={{ color: `blue`, margin: '3rem auto', maxWidth: 800 }}>
-   <h1>David Phillips</h1>
-   <p>What a world!</p>
- </div>
+import Container from "../components/container";
+
+export default () => (
+  <Container>
+     <h1>David Phillips</h1>
+     <p>What a world!</p>
+  </Container>
+)
