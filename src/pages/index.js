944aaf2628dc712865970141a45d1b5efc4ea9e1
David <davidphillips92@gmail.com>
2018-09-09 22:33:37 +1000

    fixed blank error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-import Container from "../components/container"
+//import Container from "../components/container"
 
 export default () => (
-  <Container>
+  <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}
      <h1>David Phillips</h1>
      <p>What a world!</p>
-  </Container>
+  </div>
 )
