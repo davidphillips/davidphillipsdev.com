7497a3175814500ebf30e021e6320b2c101274e9
David <davidphillips92@gmail.com>
2018-09-09 23:49:34 +1000

    another test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-//import Container from "../components/container"
+import Container from "../components/container"
 
 export default () => (
-  <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}
+  <Container>
      <h1>David Phillips</h1>
      <p>What a world!</p>
-  </div>
+  </Container>
 )
