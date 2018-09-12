fe9af1596912a468b6a26a7e5fca3a6ff7462bb3
David <davidphillips92@gmail.com>
2018-09-12 23:27:17 +1000

    removed brackets

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,8 @@ import React from "react"
 
 //import Container from "../components/container"
 
-export default () => (
+export default () =>
   <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}>
      <h1>David Phillips</h1>
      <p>What a world!</p>
   </div>
-)
