cc4ec2e355e749c21f9995ba666367de409caccb
David <davidphillips92@gmail.com>
2018-09-16 16:19:09 +1000

    added galah

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
+import galah from "../images/galah-on-fence.jpg"
 
 export default () =>
   <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}>
      <h1>David Phillips</h1>
      <p>What a world!</p>
 
-     <img src={'../images/galah-on-fence.jpg'}/>
-
+     <img src={galah} alt="Galah" />
 
   </div>
