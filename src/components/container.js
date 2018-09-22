4d4e8a8a370426157922118db64912935b0f9336
David <davidphillips92@gmail.com>
2018-09-22 18:00:39 +1000

    test

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,6 @@
-import React from "react"
+/*import React from "react"
 
 export default ({ children }) => (
   <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}>{children}</div>
 )
+*/
