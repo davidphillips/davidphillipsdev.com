30a9e8d11d797480390f6869bc63507916e143b2
David <davidphillips92@gmail.com>
2018-10-06 16:14:38 +1000

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
-import galah from "../images/galah-on-fence.jpg"
+//import galah from "../images/galah-on-fence.jpg"
+// <img src={galah} alt="Galah" />
 
 export default () =>
   <div style={{ color: `#0277BD`, margin: "3rem auto", maxWidth: 800 }}>
      <h1>David Phillips</h1>
      <p>What a world!</p>
 
-     <img src={galah} alt="Galah" />
+
 
   </div>
